58c1422da446da0f939fdccc1d29aeb4e9fa20ff
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-10 23:46:19 +0300

    getting started commit

---
+++ getting_started/app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+http.createServer((request,respose)=>{
+    // 1. Tell the browser everything is OK (Status code 200), and the data is in plain text
+    respose.writeHead(200,{
+        'Content-Type':'test/plain'
+    })
+    // 2. Write the announced text to the body of the page
+    respose.write('Hello World!\n')
+    // 3. Tell the server that all of the response headers and body have been sent
+    respose.end()
+}).listen(1234) // 4. tells the server what port to be on
---
+++ getting_started/httpServer.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+http
+  .createServer((request, respose) => {
+    // 1. Tell the browser everything is OK (Status code 200), and the data is in plain text
+    respose.writeHead(200, {
+      "Content-Type": "test/plain",
+    });
+    // 2. Write the announced text to the body of the page
+    respose.write("Hello Sensei!\n");
+    // 3. Tell the server that all of the response headers and body have been sent
+    respose.end();
+  })
+  .listen(8090); // 4. tells the server what port to be on
