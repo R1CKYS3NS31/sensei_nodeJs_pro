7c0e1063678ea8750de995c015902027e39ce4f1
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-12 00:35:36 +0300

    express routing commit

---
+++ express/router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+// routes
+app.get("home", (req, res, next) => {
+  // get codea
+});
+app.post("home", (req, res, next) => {
+  // post codes
+});
+app.put("home", (req, res, next) => {
+  // put codes
+});
+app.delete("home", (req, res, next) => {
+  // delete codes
+});
