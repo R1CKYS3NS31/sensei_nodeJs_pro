849501f69ad416b95485f3fb40cc16f38c776cfd
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-11 23:48:21 +0300

    https server commit

---
+++ httpsServer/server.js
@@ -0,0 +1,16 @@
+const https = require("https");
+const fs = require("fs");
+
+// optional
+const http = require('http')
+
+const httpsOptions = {
+  key: fs.readFileSync("ca-key.pem"), // path to server-key.pem
+  cert: fs.readFileSync("cert.pem"), // path to server-cert.pem
+};
+const app = (req, res) => {
+  res.writeHead(200);
+  res.end("Hello https sensei\n");
+};
+
+https.createServer(httpsOptions, app).listen(4000);
