9010e6d639e9f14413daeaf387ccbe6669d9e624
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-11 18:24:53 +0300

    TLS socket client commit

---
+++ tls/client.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const tls = require("tls");
+const fs = require("fs");
+
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// Pass the certs to the server and let it know to process even unauthorized certs
+const options = {
+  key: fs.readFileSync("private-key.pem"),
+  cert: fs.readFileSync("public-cert.pem"),
+  rejectUnauthorized: false, // !do not do this in production
+};
+
+const client = tls.connect(PORT,HOST,options,()=>{
+    // check if the authorization worked
+    if (client.authorized) {
+        console.log('Connection authorized by a Certificate Authority');
+    }else{
+        console.log('Connection not authorized: '+client.authorizationError);
+    }
+})
