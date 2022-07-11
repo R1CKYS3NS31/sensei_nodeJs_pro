1f472a4461cff32d7f5ae057d87d7ff006d7850f
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-11 15:51:17 +0300

    routing commit

--- routing/index.js
+++ routing/index.js
@@ -1,18 +1,28 @@
 const http = require("http");
 
-const port = 8000
-
-const index = (req, res) => {
-  res.writeHead(200);
-  res.end("Hello,Ricky");
+// routes object
+var routes = {
+  "/": (index = (req, res) => {
+    res.writeHead(200);
+    res.end("Hello,Ricky!");
+  }),
+  "/home": (home = (req, res) => {
+      res.writeHead(200)
+      res.end('Welcome home!')
+  }),
 };
 
-http.createServer((req,res)=>{
-    if (req.url==='/') {
-        return index(req,res)
+// port
+const port = 8000;
+
+http
+  .createServer((req, res) => {
+    if (req.url in routes) {
+      return routes[res.url](req, res);
     }
-    res.writeHead(404)
-    res.end(http.STATUS_CODES[404])
-}).isten(port, function () {
+    res.writeHead(404);
+    res.end(http.STATUS_CODES[404]);
+  })
+  .isten(port, () => {
     console.log("Server listening on http://localhost:" + port);
   });
