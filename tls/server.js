98636c4f50476e638aba0c0a015986fea9e5a976
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-11 17:16:18 +0300

    TLS socket server commit

--- tls/server.js
+++ tls/server.js
@@ -13,13 +13,30 @@ const options = {
 };
 
 // server
-const server = tls.createServer(options,(socket)=>{
-    // send a friendly message
-    socket.write('I am sending you a message.')
+const server = tls.createServer(options, (socket) => {
+  // send a friendly message
+  socket.write("I am sending you a message.");
 
-    // print the data that we received
-    socket.on('data', (data)=>{
-        console.log('Received: %s [it is %d bytes long]',
-        data.toString().replace(/(\n)/gm,""),data.length);
-    })
+  // print the data that we received
+  socket.on("data", (data) => {
+    console.log(
+      "Received: %s [it is %d bytes long]",
+      data.toString().replace(/(\n)/gm, ""),
+      data.length
+    );
+  });
+//   let us know when the transmission is over
+socket.listen(PORT,HOST,()=>{
+    console.log('EOT (End Of Transmission)');
+})
+});
+// start listening on a specific port and address
+server.listen(PORT,HOST,()=>{
+    console.log('listening at http://%s:%s',HOST,PORT);
+})
+// when an error occurs, show it
+server.on('error',()=>{
+    console.error(error);
+    // close the connection after the error occurred.
+    server.destroy()
 })
