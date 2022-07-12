f8a3996bea3c141d9af74fdb17c7de68d8f09c96
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-12 17:19:56 +0300

    expressJs error handling commit

--- express/index.js
+++ express/index.js
@@ -25,6 +25,21 @@ app.use(function (err, req, res, next) {
 app.get("/", function (request, response) {
   response.send("Hello, World!");
 });
+//GET /names/john
+app.get("/names/:name", function (req, res, next) {
+  if (req.params.name == "john") {
+    return res.send("Valid Name");
+  } else {
+    next(new Error("Not valid name"));
+    //pass to error handler
+  }
+});
+//error handler
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  // e.g., Not valid name
+  return res.status(500).send("Internal Server Occurred");
+});
 
 // Make the app listen on port 3000
 app.listen(port, function () {
