510fa0ca9e5cc35561981e476b10cb5454bf7b2d
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-12 17:33:45 +0300

    cookie parser commit

--- express/cookie/app.js
+++ express/cookie/app.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const cookieParser = require('cookie-parser') // module for parsing cookie
+const express = require("express");
+const cookieParser = require("cookie-parser"); // module for parsing cookie
 
-const app = express()
-app.use(cookieParser()) // parsing cookie for all routes
+const app = express();
+app.use(cookieParser()); // parsing cookie for all routes
 
-app.get('/setcookie',(req,res)=>{
-    // setting cookie
-    res.cookie('username','ricky',{
-        maxAge:900000,
-        httpOnly:true
-    })
-    return res.send('Cookie has been set')
-})
+app.get("/setcookie", (req, res) => {
+  // setting cookie
+  res.cookie("username", "ricky", {
+    maxAge: 900000,
+    httpOnly: true,
+  });
+  return res.send("Cookie has been set");
+});
 
-app.get('/getcookie',(req,res)=>{
-    const username = req.cookies['username']
-    if (username) {
-        return res.send(username)
-    }
+app.get("/getcookie", (req, res) => {
+  const username = req.cookies["username"];
+  if (username) {
+    return res.send(username);
+  }
 
-    return res.send('No cookie found')
-})
+  return res.send("No cookie found");
+});
 
-app.listen(8000)
-
+app.listen(8000);
 
 // ricky has bugs install cookie-parser
