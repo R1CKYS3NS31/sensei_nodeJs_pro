de48a43092dd1a79f492c205e210f2dd2a5e76ac
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-12 23:38:39 +0300

    filesystem i/o commit

--- filesystem_io/app.js
+++ filesystem_io/app.js
@@ -1,1 +1,24 @@
-const fs = require('fs')
+const fs = require("fs");
+const path = require("path");
+
+// with encoding
+fs.readFile("./tmp/hello.txt", { encoding: "utf-8" }, (err, content) => {
+  // if an error occurred, output it and return
+  if (err) return console.error(err);
+
+  // no error ocurred, content is a string
+  console.log(content);
+});
+
+// without encoding
+fs.readFile("binary", (err, binaryContent) => {
+  // If an error occurred, output it and return
+  if (err) return console.error(err);
+  // No error occurred, content is a Buffer, output it in
+  // hexadecimal representation.
+  console.log(content.toString("hex"));
+});
+
+fs.readFile(path.resolve(__dirname,'./tmp/hello.txt'),(err,binaryContent)=>{
+    // rest of func
+})
