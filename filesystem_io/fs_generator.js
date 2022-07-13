29648de07da543dc4db6096a148fbe4e7f6eab2d
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-13 19:06:04 +0300

    filesystem generator commit

--- filesystem_io/fs_generator.js
+++ filesystem_io/fs_generator.js
@@ -1,19 +1,23 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const run=(gen)=>{
-    const iter = gen((err,data)=>{
-        if (err) {
-            iter.throw(err)
-        }
-        return iter.next(data)
-    })
-    iter.next()
-}
-const dirPath = '/usr/local/bin'
+// Iterate through all items obtained via
+// 'yield' statements
+// A callback is passed to the generator function because it is required by
+// the 'readdir' method
+const run = (gen) => {
+  const iter = gen((err, data) => {
+    if (err) {
+      iter.throw(err);
+    }
+    return iter.next(data);
+  });
+  iter.next();
+};
+const dirPath = "/usr/local/bin";
 
 // execute the gen func
-run(function*(resume){
-    // emit the list of files in the directory from the generator
-    const contents = yield fs.readdir(dirPath,resume)
-    console.log(contents);
-})
+run(function* (resume) {
+  // emit the list of files in the directory from the generator
+  const contents = yield fs.readdir(dirPath, resume);
+  console.log(contents);
+});
