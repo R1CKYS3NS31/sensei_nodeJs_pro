9b386aa0e5a104e7dd4ea2e235765086913ccb99
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-13 20:44:54 +0300

    fs check if file exist commit

--- filesystem_io/if_file_exist.js
+++ filesystem_io/if_file_exist.js
@@ -1,21 +1,20 @@
-const fs= require('fs')
+const fs = require("fs");
 
 // asynchronously
-fs.stat('./tmp/hello.txt',(err)=>{
-    if (!err) {
-        console.log('file or directory exist');
-    }else if(err==='ENOENT'){
-        console.log('file or directory does not exist');
-    }
-})
+fs.stat("./tmp/hello.txt", (err) => {
+  if (!err) {
+    console.log("file or directory exist");
+  } else if (err === "ENOENT") {
+    console.log("file or directory does not exist");
+  }
+});
 
 // synchronously
-
 try {
-    fs.statSync('./tmp/hello.txt')
-    console.log('file or directory exist'); 
+  fs.statSync("./tmp/hello.txt");
+  console.log("file or directory exist");
 } catch (err) {
-    if (err.code==='ENOENT') {
-        console.log('file or directory does not exist');
-    }
+  if (err.code === "ENOENT") {
+    console.log("file or directory does not exist");
+  }
 }
