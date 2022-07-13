35121eaab97402c229adbc3fffe1d475330c9872
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-13 19:28:53 +0300

    fs check for permissions async commit

--- filesystem_io/check_permissions.js
+++ filesystem_io/check_permissions.js
@@ -1,22 +1,22 @@
-const fs = require('fs')
-const path = '/tmp'
+const fs = require("fs");
+const path = "/tmp";
 
 // asynchronously
 // check execution permission
-fs.access(path,fs.constants.X_OK, (err)=>{
-    if(err){
-        console.log("%s doesn't exist",path);
-    }else{
-        console.log("can execute %s",path);
-    }
-})
+fs.access(path, fs.constants.X_OK, (err) => {
+  if (err) {
+    console.log("%s doesn't exist", path);
+  } else {
+    console.log("can execute %s", path);
+  }
+});
 // Check if we have read/write permissions
 // When specifying multiple permission modes
 // each mode is separated by a pipe : `|`
-fs.access(path,fs.constants.R_OK|fs.constants.W_OK,(err)=>{
-    if (err) {
-        console.log("%s doesn't exist",path);   
-    } else {
-        console.log('can read/write %s',path);
-    }
-})
+fs.access(path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+  if (err) {
+    console.log("%s doesn't exist", path);
+  } else {
+    console.log("can read/write %s", path);
+  }
+});
