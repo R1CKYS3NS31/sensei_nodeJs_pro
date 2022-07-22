d9021537d9d5938e446f8e98c736be126451ad4c
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-22 22:16:52 +0300

    file cloning commit

--- filesystem_io/file_cloning.js
+++ filesystem_io/file_cloning.js
@@ -5,13 +5,13 @@ Create readable stream to file in current directory (__dirname) named 'node.txt'
 Use utf8 encoding
 Read the data in 16-kilobyte chunks
 */
-const readable = fs.createReadStream(__dirname + "/tmp/cloneIt.txt", {
+const readable = fs.createReadStream(__dirname + "/tmp/node.txt", {
   encoding: "utf-8",
   highWaterMark: 16 * 1024,
 });
 
 // create writable stream
-const writable = fs.createWriteStream(__dirname + "/tmp/clone.txt");
+const writable = fs.createWriteStream(__dirname + "/tmp/node_copy.txt");
 
 readable.on("data", (chunk) => {
   writable.write(chunk);
