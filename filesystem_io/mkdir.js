ac1104c4341d6e97f2f441c528f9aaffc82eec2f
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-22 21:54:59 +0300

    mkdir commit

---
+++ filesystem_io/mkdir.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+
+// asynchronous
+// const mkdir = (dirPath, callback) => {
+//   fs.mkdir(dirPath, (err) => {
+//     callback(err && err.code !== "EEXIST" ? err : null);
+//   });
+// };
+
+// mkdir("./existingDir", (err) => {
+//   if (err) return console.error(err.code);
+//   // Do something with './existingDir' here
+// });
+
+// synchronous
+const mkdirSync = (dirPath)=>{
+try {
+    fs.mkdirSync(dirPath)
+} catch (error) {
+    if(error.code !=='EEXIST') throw error
+}
+}
