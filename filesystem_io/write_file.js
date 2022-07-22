ceb41dac9d41d0750ffd26e9a53320acdc65f1fc
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-22 22:24:15 +0300

    write file commit

---
+++ filesystem_io/write_file.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+// Save the string "Hello world!" in a file called "hello.txt" in
+// the directory "/tmp" using the default encoding (utf8).
+// This operation will be completed in background and the callback
+// will be called when it is either done or failed.
+fs.writeFile("/tmp/hello_world.txt", "Hello world!", function (err) {
+  // If an error occurred, show it and return
+  if (err) return console.error(err);
+  // Successfully wrote to the file!
+});
+// Save binary data to a file called "binary.txt" in the current
+// directory. Again, the operation will be completed in background.
+
+// var buffer = Buffer.alloc([0x48, 0x65, 0x6c, 0x6c, 0x6f]);
+fs.writeFile(
+  "/tmp/binary.txt",
+  //  buffer,
+  function (err) {
+    // If an error occurred, show it and return
+    if (err) return console.error(err);
+    // Successfully wrote binary contents to the file!
+  }
+);
+
+// ricky has bugs
