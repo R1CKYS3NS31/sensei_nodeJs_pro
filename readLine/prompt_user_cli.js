a1afd13ecb8bbccc2fe297df2f4dbe2074793dc0
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-25 20:33:24 +0300

    event emitter commit

--- readLine/prompt_user_cli.js
+++ readLine/prompt_user_cli.js
@@ -4,7 +4,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
+f
 rl.question("what is your name? ", (name) => {
   console.log(`Hello ${name}`);
   rl.close();
