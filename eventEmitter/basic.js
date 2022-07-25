31a2d003dc5502ed11e496831df7f15d3ed183ea
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-07-25 20:44:58 +0300

    event emitter commit

--- eventEmitter/basic.js
+++ eventEmitter/basic.js
@@ -16,6 +16,13 @@ myDog.on("chew", (item) => {
   }
 });
 
+// when mydog is chewung, run the following function
+myDog.on("bark", () => {
+  console.log("WHO'S AT THE DOOR?");
+});
+
+// call the events and passing arguements
 myDog.emit("chew", "shoe"); // will resul in console.log(`Time to buy another ${item}`)
 const bacon = new Food();
-myDog.emit('chew',bacon) // will result in console.log("Good dog!")
+myDog.emit("chew", bacon); // will result in console.log("Good dog!")
+myDog.emit('bark')
