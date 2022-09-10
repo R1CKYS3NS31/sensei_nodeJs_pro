914551385bff5f713d613b11f806ddb1108d1ddf
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-09-10 16:02:56 +0300

    process.argv commit

--- processArgv/processArgv.js
+++ processArgv/processArgv.js
@@ -1,9 +1,11 @@
 // process.argv is an array containing the command line arguments. The first element will be node , the second
 // element will be the name of the JavaScript file. The next elements will be any additional command line arguments.
 var sum = 0
-for (i = 0; i < process.argv.length; i++) {
+for (i = 2; i < process.argv.length; i++) { // loop begins with 2 because first two elements in
+    // process.argv array always is ['path/to/node.exe', 'path/to/js/file', ...]
     // sum += Number(process.argv[i])
     sum+=parseInt(process.argv[i])
     
 }
 console.log(sum); 
+// ricky has bugs #NaN
