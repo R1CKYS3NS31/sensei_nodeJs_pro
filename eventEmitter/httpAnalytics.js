099347feb4ee8f9322c9112c8294171abbe9f18a
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-09-10 15:37:12 +0300

    event emitter http analytics commit

---
+++ eventEmitter/httpAnalytics.js
@@ -0,0 +1,15 @@
+
+
+// HTTP Analytics through an Event Emitter
+const EventEmitter = require("events");
+const serverEvents = new EventEmitter()
+
+// set up an http server
+const http = require('http')
+const httpServer = http.createServer((req,res)=>{
+    // handle the request...then emit an event about what happened
+    serverEvents.emit('request',req.method,res.url)
+})
+
+// expose the event emitter
+module.exports = serverEvents
