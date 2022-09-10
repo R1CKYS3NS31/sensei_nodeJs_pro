821ca7c9ff3fd22c3fdc97790c131e61bd5820ce
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-09-10 15:45:35 +0300

    event emitter listeners count commit

---
+++ eventEmitter/numOfListeners.js
@@ -0,0 +1,24 @@
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+var emitter = new MyEmitter();
+emitter.on("data", () => {
+  // add listener for data event
+  console.log("data event emitter");
+});
+console.log(emitter.listenerCount("data")); // => 1
+console.log(emitter.listenerCount("message")); // => 0
+emitter.on("message", function mListener() {
+  //add listener for message event
+  console.log("message event emitted");
+});
+console.log(emitter.listenerCount("data"));
+// => 1
+console.log(emitter.listenerCount("message")); // => 1
+emitter.once("data", (stuff) => {
+  //add another listener for data event
+  console.log(`Tell me my ${stuff}`);
+});
+console.log(emitter.listenerCount("data"));
+// => 2
+console.log(emitter.listenerCount("message")); // => 1
