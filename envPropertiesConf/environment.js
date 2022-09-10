a7c234e1f37c85510c8b537b23b1d0c7d4e09756
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-09-10 16:31:00 +0300

    env properties config commit

---
+++ envPropertiesConf/environment.js
@@ -0,0 +1,9 @@
+process.argv.forEach((val,index,array)=>{
+    var arg = val.split('=')
+    if (arg.length>0) {
+        if(arg[0]==='env'){
+            var env = require('/'+arg[1]+'.json')
+            module.exports=env
+        }
+    }
+})
